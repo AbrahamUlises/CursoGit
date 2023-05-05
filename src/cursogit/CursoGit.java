389defb4306b20492f9471df21a08d2465a46e7c
AbrahamUlises <L20281103@toluca.tecnm.mx>
2023-05-04 18:26:07 -0600

    Cambios en el main

--- src/cursogit/CursoGit.java
+++ src/cursogit/CursoGit.java
@@ -15,14 +15,17 @@ public class CursoGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double arr[] = {20.2, 28.6, 11.8, 3.1};
+        double arr[] = {20.8, 27.6, 1.8, 3.1};
         double sum = 0;
         double media;
+        PrbClase obj =new PrbClase("Matematicas", 1258);
+        
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
         media = sum /arr.length;
         System.out.println("La media es: "+media);
+        
     }
     
 }
